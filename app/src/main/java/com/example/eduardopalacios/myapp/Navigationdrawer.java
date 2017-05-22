f24f69006bab0fbd9210b88a4ecb804e39002e1f
aidag <aida_gil02@hotmail.es>
2017-05-22 00:10:20 -0500

    Pantalla inicio

--- app/src/main/java/com/example/eduardopalacios/myapp/Navigationdrawer.java
+++ app/src/main/java/com/example/eduardopalacios/myapp/Navigationdrawer.java
@@ -59,11 +59,22 @@ public class Navigationdrawer extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
         */
 
-       Intent intent2=getIntent();
+        //Agregar fracmento
+                FragmentManager fragmentManager=getSupportFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.content_navigationdrawer, new FragmentInicio()).commit();
+                getSupportActionBar().setTitle("Inicio");
 
-        String first_name2=intent2.getStringExtra("identificador");
+                        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+               drawer.setDrawerListener(toggle);
+               toggle.syncState();
+                NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+                navigationView.setNavigationItemSelectedListener(this);
 
+                     Intent intent2=getIntent();
 
+                String first_name2=intent2.getStringExtra("identificador");
         if(first_name2==null)
         {
 
